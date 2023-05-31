fe129ba65460e78114e973c2a3b800befe3a55f8
Touqeer Ansari <touqeeransari001@gmail.com>
2023-05-31 15:15:09 +0530

    Used arrow function

--- ArrowFunction.js
+++ ArrowFunction.js
@@ -1,5 +1,6 @@
 // console.log("Touqeer Ibn Khursheed")
 // Arrow Function in javascript
+let New=()=>'Hello world'
 const Naam="Touqeer Ibn Khursheed";
 const age=22;
-console.log(`my name is ${Naam} and my age is ${age}`);
+console.log(`my name is ${New()} and my age is ${age}`);
